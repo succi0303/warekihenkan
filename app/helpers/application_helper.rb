35496b9fcf8b207de593179f57c380395e4e2545
succi0303 <succi0303@gmail.com>
2014-01-29 23:01:36 +0900

    add facebook button refs #369

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,25 @@ module ApplicationHelper
 EOF
     return snippet
   end
+
+  def facebook_button_tag
+    snippet = <<EOF
+<div class="fb-like" data-href="http://warekihenkan.herokuapp.com/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+EOF
+    return snippet
+  end
+
+  def facebook_script_tag
+    snippet = <<EOF
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+EOF
+    return snippet
+  end
 end
