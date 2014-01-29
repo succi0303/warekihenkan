a1f2020b81af7d31b4fde4d275ebb05b25d2626d
succi0303 <succi0303@gmail.com>
2014-01-29 22:41:19 +0900

    add tweet button refs #368

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,12 @@ module ApplicationHelper
   def my_repository_path
     "https://github.com/succi0303/warekihenkan"
   end
+
+  def twitter_button_tag
+    snippet = <<EOF
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://warekihenkan.herokuapp.com/">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+EOF
+    return snippet
+  end
 end
