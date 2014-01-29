77e7a8784bc4362dc9c122e4e3ac58f88b6ab35d
succi0303 <succi0303@gmail.com>
2014-01-29 23:09:07 +0900

    add helper method for hatebu_button refs #370

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,13 @@ module ApplicationHelper
     "https://github.com/succi0303/warekihenkan"
   end
 
+  def hatena_bookmark_button_tag
+    snippet = <<EOF
+<a href="http://b.hatena.ne.jp/entry/http://warekihenkan.herokuapp.com/" class="hatena-bookmark-button" data-hatena-bookmark-title="和暦変換" data-hatena-bookmark-layout="standard-balloon" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
+EOF
+    return snippet
+  end
+
   def twitter_button_tag
     snippet = <<EOF
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://warekihenkan.herokuapp.com/">Tweet</a>
